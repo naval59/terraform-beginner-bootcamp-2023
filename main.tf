a1b2b5eb2967a508cb23900ff841ffeb724dbc8c
naval59 k <k.naval76@gmail.com>
2023-10-12 04:32:04 +0000

    #42 Custom provider WIP

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  # required_providers {
+  # terratowns = { 
+  #   source = "local.providers/local/terratowns"
+	#   version = "1.0.0"
+	#  }
+	# }
+}
   # cloud {
   #   organization = "naval59"
 
@@ -7,7 +14,7 @@ terraform {
   #   }
   # }
 
-}
+
 
 # resource "random_string" "bucket_name" {
 #   lower            = true
@@ -16,15 +23,15 @@ terraform {
 #   special          = false
 
 # }   
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
-}
+# module "terrahouse_aws" {
+#   source = "./modules/terrahouse_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   content_version = var.content_version
+#   assets_path = var.assets_path
+# }
 
 
 
