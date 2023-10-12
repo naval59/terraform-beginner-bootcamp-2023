825076d8bf64f98d11cc0d2046f91bf0b1b12e97
naval59 k <k.naval76@gmail.com>
2023-10-12 05:03:53 +0000

    #42 Created custom provider without any errors

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  # required_providers {
-  # terratowns = { 
-  #   source = "local.providers/local/terratowns"
-	#   version = "1.0.0"
-	#  }
-	# }
+  required_providers {
+  terratowns = { 
+    source = "local.providers/local/terratowns"
+	  version = "1.0.0"
+	 }
+	}
 }
   # cloud {
   #   organization = "naval59"
@@ -32,6 +32,10 @@ terraform {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
-
+provider "terratowns" {
+   endpoint = "http://localhost:4567"
+   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
 
 
--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
-output "bucket_name" {
-  description =  "bucket_name"
-  value       =  module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description =  "bucket_name"
+#   value       =  module.terrahouse_aws.bucket_name
+# }
 
-output "s3_endpoint" {
-  description =  "s3 static website endpoint url"
-  value       =  module.terrahouse_aws.s3_website_endpoint
-}
-output "cloudfront_url" {
-  description =  "This is cloud front distribution domain name"
-  value       =  module.terrahouse_aws.cloudfront_url
-}
+# output "s3_endpoint" {
+#   description =  "s3 static website endpoint url"
+#   value       =  module.terrahouse_aws.s3_website_endpoint
+# }
+# output "cloudfront_url" {
+#   description =  "This is cloud front distribution domain name"
+#   value       =  module.terrahouse_aws.cloudfront_url
+# }
