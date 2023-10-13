73923829d4b59909522013b9bcaef46b4656cc7a
naval59 k <k.naval76@gmail.com>
2023-10-13 20:13:36 +0000

    #49 changes pushed

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -1,55 +1,56 @@
 variable "user_uuid" {
-  description = "The UUID of the user."
+  description = "The UUID of the user"
   type        = string
-
   validation {
-    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.user_uuid))
-    error_message = "user_uuid must be in the UUID format (e.g., 123e4567-e89b-12d3-a456-426614174000)"
+    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message    = "The user_uuid value is not a valid UUID."
   }
 }
 
-variable "bucket_name" {
-  description = "The name of the AWS S3 bucket."
+#variable "bucket_name" {
+#  description = "The name of the S3 bucket"
+#  type        = string
+#
+#  validation {
+#    condition     = (
+#      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
+#      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
+#    )
+#    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
+#  }
+#}
 
-  # Specify the type as string.
-  type        = string
-
-  validation {
-    # Use a regular expression to validate the bucket name.
-    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
-    error_message = "The bucket name must be between 3 and 63 characters long and can only contain lowercase letters, numbers, hyphens, and periods."
-  }
-}
 variable "index_html_filepath" {
-  description = "Local file path to the index.html file."
+  description = "The file path for index.html"
   type        = string
 
   validation {
-    condition     = can(fileexists(var.index_html_filepath))
-    error_message = "The specified index_html_filepath does not exist on your local filesystem."
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exist."
   }
 }
+
 variable "error_html_filepath" {
-  description = "Local file path to the error.html file."
+  description = "The file path for error.html"
   type        = string
 
   validation {
-    condition     = can(fileexists(var.error_html_filepath))
-    error_message = "The specified error_html_filepath does not exist on your local filesystem."
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The provided path for error.html does not exist."
   }
 }
 
 variable "content_version" {
-  description = "The version of the content (positive integer starting at 1)."
+  description = "The content version. Should be a positive integer starting at 1."
   type        = number
 
   validation {
-    condition     = var.content_version >= 1 && can(regex("^[0-9]+$", tostring(var.content_version)))
-    error_message = "content_version must be a positive integer starting at 1."
+    condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
+    error_message = "The content_version must be a positive integer starting at 1."
   }
 }
-variable "assets_path" {
-  description = "Path to asset folder"
-  type        = string
-}
 
+variable "assets_path" {
+  description = "Path to assets folder"
+  type = string
+}
