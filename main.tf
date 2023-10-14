ac8d190ffcd76a246c082878c209b0c14fa031d8
naval59 k <k.naval76@gmail.com>
2023-10-14 06:21:36 +0000

    image issues fixed

--- main.tf
+++ main.tf
@@ -49,13 +49,13 @@ module "home_arcanum_hosting" {
 resource "terratowns_home" "home" {
   name = "How to play Arcanum in 2023!"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
+melomaniac-mansion is a game from 2001 that shipped with alot of bugs.
 Modders have removed all the originals making this game really fun
 to play (despite that old look graphics). This is my guide that will
 show you how to play arcanum without spoiling the plot.
 DESCRIPTION
   domain_name = module.home_arcanum_hosting.domain_name
-  town = "missingo"
+  town = "melomaniac-mansion"
   content_version = var.arcanum.content_version
 }
 
